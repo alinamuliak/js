f5deb57f79a98570350acf94da813e169801d7d4
alinamuliak <alina.muliak@ucu.edu.ua>
2022-05-10 17:40:00 +0300

    all done

--- index.js
+++ index.js
@@ -1,45 +1,85 @@
-const box = document.getElementsByClassName('box')[0];
+const boxContainer = document.getElementsByClassName('box-container')[0];
 
 const colors = ['purple', 'red', 'orange', 'green', 'pink', 'black', 'yellow', 'sky', 'blue'];
+let currentBoxId = 1;
 let leftKeyPressed = false;
-let x = 0;
-let y = 0;
+let currentTarget;
 
-// але тут має бути click походу((
-box.addEventListener('mousedown', (e) => {
-    if (e.button === 0) {
+document.addEventListener('mousedown', (e) => {
+    if (e.target && e.target.parentElement.className === 'box-container' && e.button === 0) {
         console.log('left click');
         leftKeyPressed = true;
-        x = e.offsetX;
-        y = e.offsetY;
+        currentTarget = e.target;
     }
-})
+});
 
-box.addEventListener('mouseup', () => {
-    console.log('not click');
-    if (leftKeyPressed) {
-        leftKeyPressed = false;
+
+document.addEventListener('mouseup', (e) => {
+    if (e.target && e.target.parentElement.className === 'box-container') {
+        console.log('not click');
+        if (leftKeyPressed) {
+            leftKeyPressed = false;
+            currentTarget = null;
+        }
     }
-})
+});
 
-// todo: додати, щоб мишка була посередині квадрату!!
-document.body.addEventListener('mousemove', (e) => {
-    if (leftKeyPressed) {
-        box.style.left = x + 'px';
-        box.style.top = y + 'px';
-        x = e.clientX;
-        y = e.clientY;
+
+document.addEventListener('mousemove', (e) => {
+    if (e.target && e.target.parentElement.className === 'box-container' && leftKeyPressed) {
+        currentTarget.style.transform = 'translate(-50%,-50%)';
+        currentTarget.style.left = e.pageX + 'px';
+        currentTarget.style.top = e.pageY + 'px';
     }
-})
+});
 
-box.addEventListener('contextmenu', (e) => {
-    e.preventDefault();
-    console.log('right click', box.style.backgroundColor);
-    let changedColor = colors[~~(Math.random() * colors.length)];
-    // todo: this while-loop is not working((
-    while (changedColor === box.style.backgroundColor) {
-        changedColor = colors[~~(Math.random() * colors.length)];
+
+document.addEventListener('contextmenu', (e) => {
+        e.preventDefault();
+    if (e.target && e.target.parentElement.className === 'box-container') {
+        let box = e.target;
+        console.log('right click', box.style.backgroundColor);
+        let changedColor = colors[~~(Math.random() * colors.length)];
+        while (changedColor === box.style.backgroundColor) {
+            changedColor = colors[~~(Math.random() * colors.length)];
+        }
+        box.style.backgroundColor = changedColor;
     }
-    box.style.backgroundColor = changedColor;
-})
+});
 
+
+document.addEventListener('click', (e) => {
+    if (e.target && e.target.parentElement.className === 'box-container' && e.shiftKey) {
+        console.log('shift+left click');
+        let box = e.target;
+        // if (box.classList.contains('box-large')) {
+        //     box.classList.remove('box-large');
+        // } else {
+        //     box.classList.add('box-large');
+        // }
+        box.classList.toggle('box-large');
+    }
+});
+
+
+document.addEventListener('dblclick', (e) => {
+    if (e.target && e.target.parentElement.className === 'box-container' && !e.altKey) {
+        console.log('double click');
+        ++currentBoxId;
+        const newBox = document.createElement('div');
+        newBox.classList.add('box');
+        newBox.textContent = currentBoxId.toString();
+        boxContainer.appendChild(newBox);
+    }
+});
+
+
+document.addEventListener('dblclick', (e) => {
+        console.log('alt+double click');
+    if (e.target && e.target.parentElement.className === 'box-container' && e.altKey) {
+        let box = e.target;
+        if (box.textContent !== '1') {
+            box.remove();
+        }
+    }
+});
